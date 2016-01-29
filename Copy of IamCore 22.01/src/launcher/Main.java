fed3f7f1f739f7ef61b73bf5351a532be208d20a
AdrianaSantalla <adriana.santalla@gmail.com>
2016-01-29 15:50:18 +0100

    launcher

---
+++ Copy of IamCore 22.01/src/launcher/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package launcher;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
